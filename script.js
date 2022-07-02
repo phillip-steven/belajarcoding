233e3b733769ba362c8190016ee1dfdf49605e27
I Putu Phillip Steven <putu.phillip@gmail.com>
2022-07-03 06:54:24 +0800

    Update script.js
    Menambahkan dasar dasar JS

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// Perintah menampilkan data ke web browser.
+document.write("Hello World!");
 
+/*
+Statement didalam javacscript tidak diharuskan untuk
+diakhiri titik koma. Titik koma wajib disertakan 
+ketika kita ingin menulis dua statement atau lebih dalam
+satu baris. Lebih baik seluruhnya diakhiri dengan tanda
+titik koma.
+*/
+
+// console.log digunakan untuk menampilkan data pada console
+console.log("Hello"); console.log("World!");
+
+/*
+Variabal adalah tempat untuk menampung nilai. Dalam JS,
+variabel dapat digunakan dengan keyword var, let, atau
+tanpa kata kunci.
+*/
